1d94d4dcb924078d311c6b8baf6aaa31dcb92f81
ChloeNat8 <chloegnatividad888@gmail.com>
2025-01-06 09:36:13 -0500

    Update FinalCPTChloeN.java
    Worked on UX Design and Main Menu

--- FinalCPTChloeN.java
+++ FinalCPTChloeN.java
@@ -1,10 +1,26 @@
 import arc.*;
+import java.awt.*;
+import java.awt.image.*;
+
 
 public class FinalCPTChloeN{
 	public static void main(String[] args){
 		Console con = new Console();
 		
-		con.println("Hi");
-		con.println("Hi");
+		int intBoard[];
+		int intChoice;
+		
+		// Main Menu
+		con.println("\n--- Connect 4 Main Menu ---");
+		con.println("1. Play Game");
+		con.println("2. Instructions");
+		con.println("3. Exit");
+		con.println("Enter your choice: ");
+		intChoice = con.readInt();
+            
+		
 	}
 }
+
+
+// 
